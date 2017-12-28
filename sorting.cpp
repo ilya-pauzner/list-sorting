f8f5e7fa7d1e8213c90e59cf1b8e0c12cba93c95
ilya-the-lamer <ilya-pauzner@yandex.ru>
2017-12-29 01:11:29 +0300

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -3,21 +3,23 @@
 using std::cin;
 using std::cout;
 
+// single linked list
+
 template<typename T>
 class List {
 private:
     class Node {
     public:
         T value;
-        Node *right;
+        Node *next;
 
         explicit Node(const T &v = T()) {
             value = v;
-            right = nullptr;
+            next = nullptr;
         }
 
         bool operator==(const Node &other) const {
-            return value == other.value && right == other.right;
+            return value == other.value && next == other.next;
         }
     };
 
@@ -42,7 +44,7 @@ public:
         }
 
         Iterator &operator++() {
-            cur = cur->right;
+            cur = cur->next;
             return *this;
         }
 
@@ -69,7 +71,7 @@ public:
         Node *cur = first;
         while (cur != last) {
             auto tmp = cur;
-            cur = cur->right;
+            cur = cur->next;
             delete tmp;
         }
         delete last;
@@ -77,14 +79,14 @@ public:
 
     void push_front(const T &value) {
         auto *new_first = new Node(value);
-        new_first->right = first;
+        new_first->next = first;
         first = new_first;
         ++size;
     }
 
     void pop_front() {
         Node *old_first = first;
-        first = first->right;
+        first = first->next;
         delete old_first;
         --size;
     }
@@ -113,11 +115,12 @@ size_t distance(Iter first, Iter last) {
 }
 
 template<typename Iter>
-void advance_forward(Iter &it, size_t delta) {
+Iter advance_forward(Iter it, size_t delta) {
     while (delta != 0) {
         ++it;
         --delta;
     }
+    return it;
 }
 
 template<typename Iter>
@@ -133,42 +136,34 @@ Iter copy(Iter first, Iter last, Iter out) {
 template<typename Iter>
 Iter merge(Iter a_first, Iter a_last,
            Iter b_first, Iter b_last,
-           Iter ans_first) {
-    for (; a_first != a_last; ++ans_first) {
-        if (b_first == b_last) {
-            return copy(a_first, a_last, ans_first);
-        }
-        if (*b_first < *a_first) {
-            *ans_first = *b_first;
-            ++b_first;
-        } else {
-            *ans_first = *a_first;
+           Iter out) {
+    while(a_first != a_last && b_first != b_last) {
+        if(*a_first < *b_first) {
+            *out = *a_first;
             ++a_first;
         }
+        else {
+            *out = *b_first;
+            ++b_first;
+        }
+        ++out;
     }
-    return copy(b_first, b_last, ans_first);
+    out = copy(a_first, a_last, out);
+    out = copy(b_first, b_last, out);
+    return out;
 }
 
 template<typename Iter, typename LList = typename Iter::list_type>
-void _mergesort(Iter first, Iter last, LList& buf) {
+void mergesort(Iter first, Iter last, LList &buf) {
     size_t dist = distance(first, last);
     if (dist > 1) {
-        auto middle = first;
-        advance_forward(middle, dist / 2);
-
-        _mergesort(first, middle, buf);
-        _mergesort(middle, last, buf);
-        Iter It = merge(first, middle, middle, last, buf.begin());
-        copy(buf.begin(), It, first);
+        auto middle = advance_forward(first, dist / 2);
+        mergesort(first, middle, buf);
+        mergesort(middle, last, buf);
+        Iter buf_end = merge(first, middle, middle, last, buf.begin());
+        copy(buf.begin(), buf_end, first);
     }
 }
-
-template<typename Iter, typename LList = typename Iter::list_type>
-void mergesort(Iter first, Iter last) {
-    LList buf(distance(first, last));
-    _mergesort(first, last, buf);
-}
-
 
 int main() {
     size_t n = 0;
@@ -179,7 +174,8 @@ int main() {
         cin >> elem;
         l.push_front(elem);
     }
-    mergesort(l.begin(), l.end());
+    List<int> buf(n);
+    mergesort(l.begin(), l.end(), buf);
     for (auto elem : l) {
         cout << elem << " ";
     }
